cb502ad67df792430d6dc7f9ac2c62c3f6b54f97
Troliver Russell <mrxoliver@gmail.com>
2019-09-27 15:25:59 +0100

    Create fake_data_frame.py

---
+++ fake_data_frame.py
@@ -0,0 +1,95 @@
+from typing import Any, Dict, List
+
+class FakeSeries:
+    def __init__(self, name: str, data: Dict[int, Any]):
+        self.name = name
+        self.data = data
+
+    # handle *
+    def __mul__(self, other: int) -> 'FakeSeries':
+        return FakeSeries(
+            self.name,
+            {i: v * other for i, v in self.data.items()},
+        )
+
+    # handle >
+    def __gt__(self, other: int) -> 'FakeSeries':
+        return FakeSeries(
+            self.name,
+            {i: v for i, v in self.data.items() if v > other},
+        )
+
+    # handle []
+    def __getitem__(self, key: 'FakeSeries') -> 'FakeSeries':
+        return FakeSeries(
+            self.name,
+            {i: v for i, v in self.data.items() if i in key.data},
+        )
+
+    def __repr__(self) -> str:
+        return f'<FakeSeries: {self.name} {self.data}>'
+
+
+
+class FakeDataFrame:
+    def __init__(self, d: Dict[str, List[Any]]):
+        self.series_map = {
+            k: FakeSeries(k, {i: v for i, v in enumerate(l)})
+            for k, l in d.items()
+        }
+        self.length = len(list(d.values())[0])
+
+    # handle []
+    def __getitem__(self, key: str) -> FakeSeries:
+        return self.series_map[key]
+
+    # handle [] =
+    def __setitem__(self, key: str, value: FakeSeries) -> None:
+        if key not in self.series_map:
+            self.series_map[key] = FakeSeries(key, {})
+        for i, v in value.data.items():
+            self[key].data[i] = v
+
+    def __repr__(self):
+        width = 5
+        headers = ' | '.join(
+            header.rjust(width)
+            for header in self.series_map
+        )
+        divider = '-' * len(headers)
+        rows = tuple(
+            ' | '.join(
+                str(self.series_map[k].data.get(i, 'NaN')).rjust(width)
+                    for k in self.series_map
+            )
+            for i in range(self.length)
+        )
+        return '\n'.join((headers, divider) + rows) + '\n'
+
+
+
+df = FakeDataFrame({
+    'a': [4, 5, 6],
+    'b': [7, 8, 9]
+
+})
+print(df)
+#     a |     b
+# -------------
+#     4 |     7
+#     5 |     8
+#     6 |     9
+
+print(df['b'] > 7)
+# <FakeSeries: b {1: 8, 2: 9}>
+
+print(df['a'][df['b'] > 7])
+# <FakeSeries: a {1: 5, 2: 6}>
+
+df['mult'] = df['a'][df['b'] > 7] * 2
+print(df)
+#     a |     b |  mult
+# ---------------------
+#     4 |     7 |   NaN
+#     5 |     8 |    10
+#     6 |     9 |    12
