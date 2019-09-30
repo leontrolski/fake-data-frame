aafec8fdc81e5b98c6c47187ee578b496a673386
Troliver Russell <mrxoliver@gmail.com>
2019-09-30 10:22:19 +0100

    Update fake_data_frame.py

--- fake_data_frame.py
+++ fake_data_frame.py
@@ -65,31 +65,3 @@ class FakeDataFrame:
             for i in range(self.length)
         )
         return '\n'.join((headers, divider) + rows) + '\n'
-
-
-
-df = FakeDataFrame({
-    'a': [4, 5, 6],
-    'b': [7, 8, 9]
-
-})
-print(df)
-#     a |     b
-# -------------
-#     4 |     7
-#     5 |     8
-#     6 |     9
-
-print(df['b'] > 7)
-# <FakeSeries: b {1: 8, 2: 9}>
-
-print(df['a'][df['b'] > 7])
-# <FakeSeries: a {1: 5, 2: 6}>
-
-df['mult'] = df['a'][df['b'] > 7] * 2
-print(df)
-#     a |     b |  mult
-# ---------------------
-#     4 |     7 |   NaN
-#     5 |     8 |    10
-#     6 |     9 |    12
